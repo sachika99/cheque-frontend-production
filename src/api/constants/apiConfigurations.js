d0bcea5a2f87e168341d1668f4df121af24000c0
sachika99 <sachikaherath1999@gmail.com>
2025-10-30 19:36:40 +0530

    adding loging fordet password  logout and  secure routes

--- src/api/constants/apiConfigurations.js
+++ src/api/constants/apiConfigurations.js
@@ -1,5 +1,9 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 export const API_CONFIGURATIONS = {
-  
+    REGISTER_USER: `${BASE_URL}/register`,
+    LOGIN_USER: `${BASE_URL}/login`,
+    FORGOT_PASSWORD_USER: `${BASE_URL}/forgot-password`,
+    RESET_PASSWORD_USER: `${BASE_URL}/reset-password`,
+    LOGOUT_USER: `${BASE_URL}/logout`,
 };
